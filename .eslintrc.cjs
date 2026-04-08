9aab6dff9cee9184df421381e99ec899467ae9c7
u7663394 <u7663394@anu.edu.au>
2026-04-08 22:23:14 +1000

    init project via pnpm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,36 @@
+const { rules } = require('eslint-plugin-vue')
+
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true, // 单引号
+        semi: false, // 无分号
+        printWidth: 100, // 每行最大宽度
+        trailingComma: 'none', // 无尾逗号
+        endOfLine: 'auto' // 换行符
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index'] // 允许 index.vue 命名
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'], // 允许 props 解构
+    'no-undef': 'error' // 禁止使用未定义的变量
   }
 }
